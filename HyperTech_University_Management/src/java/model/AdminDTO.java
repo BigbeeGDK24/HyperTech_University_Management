4ae7a8d7ffec3ccb85d94515e2493f64f7a4b237
TruongHN192908 <truonghuynhkof123@gmail.com>
2026-02-27 10:06:30 +0700

    up

---
+++ HyperTech_University_Management/src/java/model/AdminDTO.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author truon
+ */
+public class AdminDTO {
+    private String Username;
+    private String Password;
+
+    public AdminDTO() {
+    }
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setUsername(String Username) {
+        this.Username = Username;
+    }
+
+    public void setPassword(String Password) {
+        this.Password = Password;
+    }
+    
+}
