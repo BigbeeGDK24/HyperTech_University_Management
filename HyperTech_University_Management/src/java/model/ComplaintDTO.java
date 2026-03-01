a604478aa50bc9935a16b2ff616a56ca824d69cc
duonganhkiet10a11-art <duonganhkiet10a11@gmail.com>
2026-03-01 16:25:17 +0700

    up

---
+++ HyperTech_University_Management/src/java/model/ComplaintDTO.java
@@ -0,0 +1,89 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author ASUS
+ */
+public class ComplaintDTO {
+    private int id;
+    private String userId;
+    private int orderId;
+    private int productId;
+    private String title;
+    private String content;
+    private String status;
+
+    public ComplaintDTO() {
+    }
+
+    public ComplaintDTO(int id, String userId, int orderId, int productId, String title, String content, String status) {
+        this.id = id;
+        this.userId = userId;
+        this.orderId = orderId;
+        this.productId = productId;
+        this.title = title;
+        this.content = content;
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    
+}
